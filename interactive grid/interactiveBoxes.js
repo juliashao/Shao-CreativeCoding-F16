19216026b4e611573dd1a915a7f63ba9ebb25869
juliashao <jxs206@nyu.edu>
2016-11-15 14:43:21 -0500

    .

--- interactive grid/interactiveBoxes.js
+++ interactive grid/interactiveBoxes.js
@@ -7,30 +7,29 @@ function setup() {
 function draw(){
 
   if (mouseX <= 300 && mouseY <= 166){
-    if (mouseisPressed){
+    if (mouseIsPressed)
       fill(144,232,152);
       rect(0,0,300,166);
-    }
+    
   }
    
   if (mouseX <=300 && mouseY >166){
-    if(mouseisPressed){
+    if(mouseIsPressed)
       fill(255,0,117);
       rect(0,166,300,334);
-    }
+    
   }
   
   if (mouseX > 300){
-     if (mouseisPressed){
+     if (mouseIsPressed)
        fill(255,131,102);
        rect(300,0,300,height);
-     }
+    
   }
   
   if (mouseX >500){
-    if (mouseisPressed){
+    if (mouseIsPressed)
       fill(136,176,232);
       rect(500,0,100,height);
-    }
   }
 }
